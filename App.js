1f0ab18ba38ae62ba0b90f33600a413844d723b1
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-06-23 01:39:31 +0100

    carousel

--- App.js
+++ App.js
@@ -1,27 +1,74 @@
-import React, { Component } from "react";
-import { Text, View } from 'react-native';
-import Carousel from "react-native-snap-carousel";
-class MyCarousel extends Component {
-  _renderItem = ({ item, index }) => {
-    return (
-      <View style={styles.slide}>
-        <Text style={styles.title}>{item.title}</Text>
-      </View>
-    );
-  };
+import * as React from 'react';
+import {
+  Text, 
+  View,
+  SafeAreaView } from 'react-native';
 
-  render() {
-    return (
-      <Carousel
-        ref={(c) => {
-          this._carousel = c;
-        }}
-        data={this.state.entries}
-        renderItem={this._renderItem}
-        sliderWidth={sliderWidth}
-        itemWidth={itemWidth}
-      />
-    );
-  }
+import Carousel from 'react-native-snap-carousel';
+
+export default class App extends React.Component {
+
+ 
+    constructor(props){
+        super(props);
+        this.state = {
+          activeIndex:0,
+          carouselItems: [
+          {
+              title:"Item 1",
+              text: "Text 1",
+          },
+          {
+              title:"Item 2",
+              text: "Text 2",
+          },
+          {
+              title:"Item 3",
+              text: "Text 3",
+          },
+          {
+              title:"Item 4",
+              text: "Text 4",
+          },
+          {
+              title:"Item 5",
+              text: "Text 5",
+          },
+        ]
+      }
+    }
+
+    _renderItem({item,index}){
+        return (
+          <View style={{
+              backgroundColor:'floralwhite',
+              borderRadius: 5,
+              height: 250,
+              padding: 50,
+              marginLeft: 25,
+              marginRight: 25, }}>
+            <Text style={{fontSize: 30}}>{item.title}</Text>
+            <Text>{item.text}</Text>
+          </View>
+
+        )
+    }
+
+    render() {
+        return (
+          <SafeAreaView style={{flex: 1, backgroundColor:'rebeccapurple', paddingTop: 50, }}>
+            <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center', }}>
+                <Carousel
+                  layout={"default"}
+                  ref={ref => this.carousel = ref}
+                  data={this.state.carouselItems}
+                  sliderWidth={300}
+                  itemWidth={300}
+                  renderItem={this._renderItem}
+                  onSnapToItem = { index => this.setState({activeIndex:index}) } />
+            </View>
+          </SafeAreaView>
+        );
+    }
 }
-export default MyCarousel;
+
